58e693b2bcbac8938b1fcdf96e51492141253607
Night_Fox <74563879+Valerya-Teplova@users.noreply.github.com>
2021-11-23 12:56:20 +0300

    Д.З. 1
    Создание репозитория на гит; первая программа на питон.

---
+++ File 1.py
@@ -0,0 +1,9 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print "Hello world"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Hello world")?
+>>> print 'Hello world'
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print('Hello world')?
+>>> print ('Hello world')
+Hello world
+>>> 
